2d2cae2c01b5f9f177de09128125f281baaf2f2a
Jordan Hurt <jth0024@gmail.com>
2016-01-12 22:02:44 -0600

    Initial commit
    Initial commit

---
+++ client/webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.less$/,
+        loader: "style!css!less"
+      }
+    ]
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
